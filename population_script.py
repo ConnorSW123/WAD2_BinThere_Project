0c05a11d5bcbb999d818c8e4abff6a9a10543110
ConnorSW123 <2880595S@student.gla.ac.uk>
2025-03-09 15:52:12 +0000

    Update population_script.py

--- population_script.py
+++ population_script.py
@@ -8,14 +8,14 @@ from django.contrib.auth.models import User
 from BinThere.models import Location, BinType, Bin, Vote
 
 def populate():
-    # First, create some users (or ensure they exist)
+    # First, create some users
     user1 = User.objects.get_or_create(username="user1")[0]
     user2 = User.objects.get_or_create(username="user2")[0]
     user3 = User.objects.get_or_create(username="user3")[0]
 
     print("Users populated!")
 
-    # Now create some locations (or ensure they exist)
+    # Now create some locations
     loc1 = Location.objects.get_or_create(name="University of Glasgow", latitude="55.8780", longitude="-4.2900")[0]
     loc2 = Location.objects.get_or_create(name="Kelvingrove Park", latitude="55.8701", longitude="-4.2855")[0]
     loc3 = Location.objects.get_or_create(name="Botanic Gardens", latitude="55.8764", longitude="-4.2952")[0]
@@ -24,7 +24,7 @@ def populate():
 
     print("Locations populated!")
 
-    # Now create some bin types (or ensure they exist)
+    # Now create some bin types
     pet_bin_type = BinType.objects.get_or_create(name="PET Bottle")[0]
     glass_bin_type = BinType.objects.get_or_create(name="Glass")[0]
     plastic_bin_type = BinType.objects.get_or_create(name="Soft Plastic")[0]
@@ -42,7 +42,7 @@ def populate():
 
     print("Bins populated!")
 
-    # Now add some votes for the bins (this will just add votes and not update upvote/downvote counters directly)
+    # Now add some allocated votes to users for the bins
     votes_data = [
         {'bin': bin1, 'user': user1, 'vote': 1},  # Upvote for bin 1
         {'bin': bin2, 'user': user2, 'vote': -1}, # Downvote for bin 2
