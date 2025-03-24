ca49714ae1fcc4b7a2a7a4fdff92f5a33372c7ce
Connor Sweeney <2880595S@student.gla.ac.uk>
2025-03-24 00:40:24 +0000

    Secret Key Implementation

--- WAD2_BinThere_Project/settings.py
+++ WAD2_BinThere_Project/settings.py
@@ -31,6 +31,10 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+key = None 
+with open('secret.key') as f: 
+    key = f.read().strip() 
+    SECRET_KEY = key
 SECRET_KEY = 'lvnh*)sjrilx$^5v(ee-j3+ucdnvbzs5i%0e%c(hz^2ewh&a4f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
